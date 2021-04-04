4aec61fe333cf9e90c19d4fbdb6f1fc637a1ee6a
jerrypvnrt <my072814638@gmail.com>
2021-04-04 19:52:52 +0800

    Program Design Practice add .gitignore

--- Program_Design/109-2/practice/13_Transcript_using_Linked_List/term_counting_by_linked_list.c
+++ Program_Design/109-2/practice/13_Transcript_using_Linked_List/term_counting_by_linked_list.c
@@ -4,18 +4,16 @@
 #include <ctype.h>
 #include <string.h>
 #define MaxWordLen 1024
-#define HWCnt 5
 
 typedef struct NODE {
     char *name;
-    int *score;
-    int sum;
+    int count;
     struct NODE *next;
     struct NODE *pre;
 } Node;
 
-Node *insert(Node *, char *, int *);   // return head
-Node *find(Node *, int);     // return the location of key, or NULL if not find.
+Node *insert(Node *, char *);   // return head
+Node *find(Node *, char *);     // return the location of key, or NULL if not find.
 Node *seek_tail(Node *);        // return the tail of the list, or NULL if list is NULL.
 void print(Node *);
 void print_reverse(Node *);
@@ -25,14 +23,34 @@ void print_reverse(Node *);
 int main() {
     setvbuf(stdout, NULL, _IOFBF, 4096);
     Node *list = NULL;
+    Node *list_last;
+    Node *seek;
     char name[MaxWordLen];
-    int score[HWCnt];
+    char *nameptr;
+    bool delete;
 
-    while ( ~scanf("%s %d %d %d %d %d", name,\
-            &score[0], &score[1], &score[2], &score[3], &score[4]) ) {
-        list = insert(list, name, score);
+    while (fgets(name, MaxWordLen, stdin) != NULL) {
+        if (name[strlen(name) - 1] == '\n') {
+            name[strlen(name) - 1] =  '\0';
+        }
+
+        nameptr = name;
+        delete = false;
+
+        if (*nameptr == '-') {
+            delete = true;
+            ++nameptr;
+        }
+
+        if ((seek = find(list, nameptr)) == NULL) {
+            list = insert(list, nameptr);
+        } else if (delete) {
+            --(seek->count);
+        } else {
+            ++(seek->count);
+        }
     }
-
+    
     print(list);
 
     return 0;
@@ -40,18 +58,13 @@ int main() {
 /////////////////////////////////////////////////////////
 
 
-Node *insert(Node *head, char *key, int *score) {
+Node *insert(Node *head, char *key) {
     Node *newNode;
     Node *traverseptr;
 
     newNode = (Node *)malloc(sizeof(Node));
-    newNode->sum = 0;
     newNode->name = strdup(key);
-    newNode->score = calloc(HWCnt, sizeof(int));
-    for (int i = 0; i < HWCnt; ++i) {
-        newNode->score[i] = score[i];
-        newNode->sum += score[i];
-    }
+    newNode->count = 1;
     newNode->next = NULL;
     newNode->pre = NULL;
 
@@ -59,31 +72,18 @@ Node *insert(Node *head, char *key, int *score) {
         return newNode;
     }
 
-    traverseptr = find(head, newNode->sum);
-    if (traverseptr == head) {  // insert at head
-        traverseptr->pre = newNode;
-        newNode->next = traverseptr;
-        newNode->pre = NULL;
-        return newNode;
-    } else if (traverseptr != head && traverseptr) {
-        traverseptr->pre->next = newNode;
-        newNode->pre = traverseptr->pre;
-        newNode->next = traverseptr;
-        traverseptr->pre = newNode;
-        return head;
-    } else {  // insert at tail
-        traverseptr = seek_tail(head);
-        traverseptr->next = newNode;
-        newNode->next = NULL;
-        newNode->pre = traverseptr;
-        return head;
-    }
+    traverseptr = seek_tail(head);
+    traverseptr->next = newNode;
+    newNode->next = NULL;
+    newNode->pre = traverseptr;
+    
+    return head;
 }
 
-Node *find(Node *head, int sum) {
+Node *find(Node *head, char *key) {
     Node *traverse = head;
     while (traverse) {
-        if (traverse->sum < sum) {
+        if (!strcmp(traverse->name, key)) {
             return traverse;
         }
         traverse = traverse->next;
@@ -106,36 +106,26 @@ Node *seek_tail(Node *head) {
 
 void print(Node *head) {
     Node *traverse;
-    int i;
 
     if (head == NULL) {
         printf("NULL\n");
     }
     traverse = head;
     while (traverse) {
-        printf("%s\t", traverse->name);
-        for (i = 0; i < HWCnt - 1; ++i) {
-            printf("%d\t", traverse->score[i]);
-        }
-        printf("%d\n", traverse->score[i]);
+        printf("%s: %d\n", traverse->name, traverse->count);
         traverse = traverse->next;
     }
 }
 
 void print_reverse(Node *head) {
     Node *tail;
-    int i;
 
     if (head == NULL) {
         printf("NULL\n");
     }
     tail = seek_tail(head);
     while (tail) {
-        printf("%s\t", tail->name);
-        for (i = 0; i < HWCnt - 1; ++i) {
-            printf("%d\t", tail->score[i]);
-        }
-        printf("%d\n", tail->score[i]);
+        printf("%s: %d\n", tail->name, tail->count);
         tail = tail->pre;
     }
 }
