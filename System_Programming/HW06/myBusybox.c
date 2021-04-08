de5f4802f9e5c903d90e350dc8614ef17b04e980
jerrypvnrt <my072814638@gmail.com>
2021-04-08 17:59:52 +0800

    System Programming HW06 ver.2

--- System_Programming/HW06/myBusybox.c
+++ System_Programming/HW06/myBusybox.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libgen.h>
+#define MaxBufLen 4096
 
 int main(int argc, char **argv) {
-    char exeBuf[4096];
+    char exeBuf[MaxBufLen];
     char *exename = basename(argv[0]);
+    int inputLen = MaxBufLen;
 
     strcpy(exeBuf, exename);
+    inputLen -= strlen(exename);
     for (int i = 1; i < argc; ++i) {
         strcat(exeBuf, " ");
         strcat(exeBuf, argv[i]);
+        inputLen -= (strlen(argv[i]) + 1);
+        if (inputLen < 0) {
+            fprintf(stderr, "Too long\nMax buffer len is %d", MaxBufLen);
+            return 1;
+        }
     }
 
     system(exeBuf);
