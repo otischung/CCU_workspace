35ccb15d44501dcbc68cbef73b96fa17919429ef
jerrypvnrt <my072814638@gmail.com>
2021-04-15 11:33:06 +0800

    System Programming HW07 inotify.c add NAME_MAX

--- System_Programming/HW07/inotify.c
+++ System_Programming/HW07/inotify.c
@@ -1,11 +1,12 @@
-#include <sys/inotify.h>
 #include <stdio.h>
-#include <dirent.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
+// #include <dirent.h>
+#include <sys/inotify.h>
 
+#define NAME_MAX 8192
 #define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 
 void printInotifyEvent(struct inotify_event* event);
@@ -14,30 +15,30 @@ char wd[1000][10000];
 
 int main(int argc, char **argv) {
 	int fd, num, ret, i;
-	char* p;
+	char *p;
 	char inotify_entity[BUF_LEN];
 	fd = inotify_init();
-	for (i=1; i< argc; i++) {
-		ret=inotify_add_watch(fd, argv[i], IN_ALL_EVENTS);
+	for (i = 1; i < argc; ++i) {
+		ret = inotify_add_watch(fd, argv[i], IN_ALL_EVENTS);
 		strcpy(wd[ret], argv[i]);
 	}
-	while(1) {
+	while (true) {
 		num = read(fd, inotify_entity, BUF_LEN);
 		for (p = inotify_entity; p < inotify_entity + num; ) {
-			printInotifyEvent((struct inotify_event *) p);
-			p+=sizeof(struct inotify_event) + ((struct inotify_event *)p)->len;
+			printInotifyEvent((struct inotify_event *)p);
+			p += sizeof(struct inotify_event) + ((struct inotify_event *)p)->len;
 		}
 	}
 }
 
-void printInotifyEvent(struct inotify_event* event) {
-	char buf[4096]="";
+void printInotifyEvent(struct inotify_event *event) {
+	char buf[4096] = "";
 	sprintf(buf, "[%s] ", wd[event->wd]);
 	strncat(buf, "{", 4096);
 	if (event->mask & IN_ACCESS) 		strncat(buf, "ACCESS, ", 4096);
 	if (event->mask & IN_ATTRIB) 		strncat(buf,"ATTRIB, ", 4096);
 	if (event->mask & IN_CLOSE_WRITE) 	strncat(buf,"CLOSE_WRITE, ", 4096);
-	if (event->mask & IN_CLOSE_NOWRITE) 	strncat(buf,"CLOSE_NOWRITE, ", 4096);
+	if (event->mask & IN_CLOSE_NOWRITE) strncat(buf,"CLOSE_NOWRITE, ", 4096);
 	if (event->mask & IN_CREATE) 		strncat(buf,"CREATE, ", 4096);
 	if (event->mask & IN_DELETE) 		strncat(buf,"DELETE, ", 4096);
 	if (event->mask & IN_DELETE_SELF) 	strncat(buf,"DELETE_SELF, ", 4096);
@@ -49,10 +50,10 @@ void printInotifyEvent(struct inotify_event* event) {
 	if (event->mask & IN_IGNORED)		strncat(buf,"IGNORED, ", 4096);
 	if (event->mask & IN_ISDIR)			strncat(buf,"ISDIR, ", 4096);
 	if (event->mask & IN_Q_OVERFLOW)	strncat(buf,"Q_OVERFLOW, ", 4096);
-	buf[strlen(buf)-2]='\0';
+	buf[strlen(buf) - 2] = '\0';
 	strncat(buf, "}", 4096);
 	sprintf(buf, "%s cookie=%d", buf, event->cookie);
-	if (event->len>0)
+	if (event->len > 0)
     	sprintf(buf, "%s name = %s\n", buf, event->name);
 	else
 		sprintf(buf, "%s name = null\n", buf);
