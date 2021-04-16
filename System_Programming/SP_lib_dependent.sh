64c385e3ec1bd4221a739f5db20286f08752a32b
jerrypvnrt <my072814638@gmail.com>
2021-04-16 18:31:47 +0800

    System Programming add SP_lib_dependent.sh

---
+++ System_Programming/SP_lib_dependent.sh
@@ -0,0 +1,35 @@
+# HW04
+# utrace: gcc -pg
+# ltrace: -z lazy
+
+# HW05
+# 
+
+# HW06 busybox
+# git clone https://git.busybox.net/busybox/
+# 這是 make menuconfig 時需要的函數庫
+sudo apt install -y libncurses5-dev
+# cd busybox
+# make defconfig
+# make menuconfig
+# make
+# make install
+# sudo chroot ~/busybox/_install/ ash
+# cd ..
+
+# HW07
+# ACL = access control list
+sudo apt install -y libacl1-dev
+# inotify 監控檔案或目錄的讀寫
+sudo apt install -y inotify-tools
+# ACL GUI
+sudo apt install -y eiciel
+sudo apt install -y acl acl-dev
+sudo apt install -y clang
+# 擴充屬性
+sudo apt install -y attr
+# 另一種 debugger，這是基於 KDE 的函數庫寫出來的程式，故程式雖小，安裝需花較多時間
+# sudo apt install -y kdbg
+# 試用 inotify
+sudo apt install -y inotify-tools
+
