1b7a3024938b787bec002222dfde0eced7fcc787
jerrypvnrt <my072814638@gmail.com>
2021-05-01 09:47:05 +0800

    SP HW08 ver.1

--- System_Programming/HW08/nice_testing.c
+++ System_Programming/HW08/nice_testing.c
@@ -14,9 +14,11 @@ unsigned long long count = 0;
 void alarmHandler(int);
 
 int main(int argc, char **argv) {
+    short success;
     int nice_v;
+    long cputhreads;  // The amount of threads of CPU
     pid_t childPid;
-    cpu_set_t set;
+    cpu_set_t mask;  // The set of the CPU threads.
 
     if (argc != 2 && !isdigit(argv[1][0]) && argv[1][0] != '-' && argv[1][0] != '+') {
         fprintf(stderr, "format error\n");
@@ -24,10 +26,18 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    CPU_ZERO(&set);
-    CPU_SET(0, &set);
-    // long num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-    // printf("%ld\n", num_cpus);
+    //    This block of code is to set CPU affinity to thread 0 only    //
+    cputhreads = sysconf(_SC_NPROCESSORS_CONF);
+    CPU_ZERO(&mask);
+    CPU_SET(0, &mask);
+    // CPU_SET(1, &mask);
+    success = sched_setaffinity(0, sizeof(mask), &mask);
+    if (success != 0) {
+        perror("setaffinity failed");
+        return 1;
+    }
+    // Reference: https://blog.csdn.net/Z_Stand/article/details/107883684
+    //////////////////////////////////////////////////////////////////////
     
     nice_v = atoi(argv[1]);
     childPid = fork();
@@ -43,7 +53,7 @@ int main(int argc, char **argv) {
     }
 
     signal(SIGALRM, alarmHandler);
-    alarm(1);
+    alarm(10);  // After 10 seconds, this process will be terminated
 
     while (true) ++count;
 
