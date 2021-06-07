9f9d5f496f853a149263b5b93071e5c7ea21259b
jerrypvnrt <my072814638@gmail.com>
2021-06-07 19:31:24 +0800

    Program Design practice add 18_copy ver.0

---
+++ Program_Design/109-2/practice/18_Copy/copy.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void usage(char *progname);
+
+
+////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv) {
+    off_t fileSize;
+    int readfd, writefd;
+    int ret;
+    char *buf;
+
+    if (argc != 3) usage(argv[0]);
+
+    readfd = open(argv[1], O_RDONLY, S_IRUSR);
+    if (readfd < 0) perror("readfd open failed");
+
+    fileSize = lseek(readfd, 0, SEEK_END);
+    buf = (char *)malloc(fileSize * sizeof(char));
+    lseek(readfd, 0, SEEK_SET);
+
+    ret = read(readfd, buf, fileSize);
+
+    writefd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (writefd < 0) perror("writefd open failed");
+
+    ret = write(writefd, buf, fileSize);
+
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////
+
+
+void usage(char *progname) {
+    fprintf(stderr, "format error.\nusage: %s <src> <dst>\n", progname);
+    exit(1);
+}
