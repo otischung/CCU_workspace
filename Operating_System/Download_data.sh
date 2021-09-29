e82c2397fa0a260cb5d363aa1b9d4bfcb41ae409
otischung <my072814638@gmail.com>
2021-09-29 10:28:18 +0800

    OS add download script

---
+++ Operating_System/Download_data.sh
@@ -0,0 +1,7 @@
+wget -O "osdi.tar.xz" "https://uc72d67208bef6e3ddb57083ec4c.dl.dropboxusercontent.com/cd/0/get/BXBiwTpDAneCCwpryAZ5LZVTaAezhasaKU5faG8t6P-w9qq3EZfibxB09hzZBtI38ANdWmIoAJoF7_qjXA0830rKSIl2acVQDs_Z4UKRREMR_TwtEn1ZT9nPID5w5cbNjHdzbiMBb_l-CUuKuIDjpvcV/file?_download_id=2282592997514465496082683023993681556484588936153405011318575351842&_notify_domain=www.dropbox.com&dl=1"
+tar -xvf osdi.tar.xz
+rm osdi.tar.xz
+
+wget -O "cdt-stand-alone-debugger.tar.gz" "https://mirror.library.ucy.ac.cy/eclipse/tools/cdt/releases/10.1/cdt-10.1.0/rcp/cdt-stand-alone-debugger-10.1.0-20201211-1436-linux.gtk.x86_64.tar.gz"
+tar zxvf cdt-stand-alone-debugger.tar.gz
+rm cdt-stand-alone-debugger.tar.gz
