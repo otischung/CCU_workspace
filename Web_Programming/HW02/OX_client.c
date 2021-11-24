195185ce7b0839272517738870adb96cf72d1b6b
otischung <my072814638@gmail.com>
2021-11-24 12:33:00 +0800

    HW02 init3 struggle with message exchange using socket

--- Web_Programming/HW02/OX_client.c
+++ Web_Programming/HW02/OX_client.c
@@ -1,51 +1,63 @@
-#include <strings.h>
-#include <time.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#define SA struct sockaddr
-#define MAXLINE 4096
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+#define forever while (1)
+#define MAXLINE 8192
 
 int main(int argc, char **argv) {
-    int sockfd, n;
-    char recvline[MAXLINE + 1];
+    int socket_fd;
+    FILE *socket_fp;
+    ssize_t n;
+    char ip_addr[1024];
+    char recvline[MAXLINE];
     struct sockaddr_in servaddr;
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s, <IP address>\n", argv[0]);
+    if (argc == 1) {
+        strcpy(ip_addr, "127.0.0.1");
+    } else if (argc == 2) {
+        strcpy(ip_addr, argv[1]);
+    } else {
+        fprintf(stderr, "usage: %s, [IP address]\n", argv[0]);
         exit(1);
     }
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket error");
         exit(1);
     }
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(13);
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+    servaddr.sin_port = htons(8000);
+    if (inet_pton(AF_INET, ip_addr, &servaddr.sin_addr) <= 0) {
         perror("inet_pton error");
         exit(1);
     }
-    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0) {
+    if (connect(socket_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("connect error");
         exit(1);
     }
-    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
-        recvline[n] = 0;
-        if (fputs(recvline, stdout) == EOF) {
-            perror("fputs error");
-            exit(1);
-        }
+
+    socket_fp = fdopen(socket_fd, "a+");
+    setvbuf(socket_fp, NULL, _IONBF, 0);
+    forever {
+        fgets(recvline, MAXLINE, socket_fp);
+        printf("%s", recvline);
+        scanf("%s", recvline);
+        fprintf(socket_fp, "%s", recvline);
+        fgets(recvline, MAXLINE, socket_fp);
+        printf("%s", recvline);
     }
-    if (n < 0) {
-        perror("read error");
-        exit(1);
-    }
-    
+
+    fclose(socket_fp);
+    shutdown(socket_fd, SHUT_RDWR);
+    close(socket_fd);
+
     return 0;
 }
--- Web_Programming/HW02/OX_server.c
+++ Web_Programming/HW02/OX_server.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,17 +11,20 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #define MAXLINE 8192
+#define forever while (1)
 
+/* ---------------- Define Matrix Operation ---------------- */
 // O: 0, X: 1
 short matrix[3][3];
 
-#define cleanMatrix() ({              \
-    for (int i = 0; i < 3; ++i) {     \
-        for (int j = 0; j < 3; ++j) { \
-            matrix[i][j] = -1;        \
-        }                             \
-    }                                 \
-})
+#define cleanMatrix()                     \
+    do {                                  \
+        for (int i = 0; i < 3; ++i) {     \
+            for (int j = 0; j < 3; ++j) { \
+                matrix[i][j] = -1;        \
+            }                             \
+        }                                 \
+    } while (0)
 
 #define dumpMatrix(fp)                            \
     do {                                          \
@@ -54,17 +58,21 @@ short matrix[3][3];
         fprintf((fp), " 2 0 | 2 1 | 2 2\n");                   \
     } while (0)
 
+/* ---------------- Define Player's Information ---------------- */
+typedef struct PLAYER {
+    char name[MAXLINE];
+    bool status;  // online or not
+} Player;
+
 int main() {
     int listen_fd, connect_fd;
     FILE *connect_fp;
     struct sockaddr_in servaddr;  // <netinet/in.h>
     int ret;
-    pid_t pid;
     unsigned val = 1;
+    char username[MAXLINE];
 
     cleanMatrix();
-    dumpMatrix(stdout);
-    usage(stdout);
     listen_fd = socket(AF_INET, SOCK_STREAM, 0);  // IPv4, TCP  <sys/socket.h>
     setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
     bzero(&servaddr, sizeof(servaddr));  // <strings.h>
@@ -82,20 +90,26 @@ int main() {
         exit(1);
     }
 
-    while (1) {
+    forever {
         connect_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL);  // <sys/socket.h>
-        if (connect_fd > 0) {
-            fprintf(stderr, "connection accept\n");
-        } else {
+        if (connect_fd < 0) {
             perror("accept error");
             continue;
         }
-        connect_fp = fdopen(connect_fd, "w+");
 
-        /*---Close the socket---*/
-        fclose(connect_fp);
-        // shutdown(connect_fd, SHUT_RDWR);
-        close(connect_fd);
+        connect_fp = fdopen(connect_fd, "a+");
+        setvbuf(connect_fp, NULL, _IONBF, 0);
+        fprintf(connect_fp, "Please login to play:\n");
+        // fgets(username, MAXLINE, connect_fp);
+        fscanf(connect_fp, "%s", username);
+        fprintf(stdout, "Your name is %s\n", username);  // test
+        fprintf(connect_fp, "Your name is %s\n", username);
+        getchar();
     }
+
+    /*---Close the socket---*/
+    fclose(connect_fp);
+    // shutdown(connect_fd, SHUT_RDWR);
+    close(connect_fd);
     return 0;
 }
