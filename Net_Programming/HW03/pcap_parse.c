b449cab1a410a08030def24311aab77dc9ded365
otischung <my072814638@gmail.com>
2021-12-16 15:23:14 +0800

    HW03 init2 grab a packet and print its length

--- Net_Programming/HW03/pcap_parse.c
+++ Net_Programming/HW03/pcap_parse.c
@@ -1,12 +1,18 @@
+// Reference: https://www.tcpdump.org/pcap.html
 #include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
 int main(int argc, char **argv) {
     // pcap_if_t *alldevs;
     pcap_t *readpcap;
+    struct bpf_program fp;
     char errbuf[PCAP_ERRBUF_SIZE];  // PCAP_ERRBUF_SIZE = 256
+    char filter_exp[] = "port 53";	/* The filter expression */
+    struct pcap_pkthdr header;	/* The header that pcap gives us */
+	const u_char *packet;		/* The actual packet */
     int ret;
 
     memset(errbuf, '\0', PCAP_ERRBUF_SIZE * sizeof(char));
@@ -30,9 +36,26 @@ int main(int argc, char **argv) {
     
     readpcap = pcap_open_offline(argv[1], errbuf);
     if (readpcap == NULL) {
-        fprintf(stderr, "%s\n", errbuf);
+        fprintf(stderr, "Couldn't open file %s: %s\n", argv[1], errbuf);
+        return EXIT_FAILURE;
     }
-    
+    ret = pcap_compile(readpcap, &fp, filter_exp, 1, 0);
+    if (ret == PCAP_ERROR) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(readpcap));
+        return EXIT_FAILURE;
+    }
+    ret = pcap_setfilter(readpcap, &fp);
+    if (ret == PCAP_ERROR) {
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(readpcap));
+        return EXIT_FAILURE;
+    }
+
+    /* Grab a packet */
+	packet = pcap_next(readpcap, &header);
+	/* Print its length */
+	printf("Jacked a packet with length of [%d]\n", header.len);
+	/* And close the session */
+	pcap_close(readpcap);
 
 
 
